e18aaade7c712ad0f9cc09d5c62e0a228f0a9fe0
Eric-Yao <norhustar@homtial.com>
2020-11-17 19:01:12 +0100

    Create plat_math.sol

---
+++ contracts/plat_math.sol
@@ -0,0 +1,51 @@
+/*
+Copyright (c) 2016 Smart Contract Solutions, Inc.
+Original: https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/contracts/math/SafeMath.sol
+*/
+
+pragma solidity ^0.4.26;
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint a, uint b) internal pure returns (uint c) {
+        if (a == 0) {
+            revert();
+        }
+        c = a * b;
+        require(c / a == b);
+    }
+
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint a, uint b) internal pure returns (uint c) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        // uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        require(b > 0);
+        c = a / b;
+    }
+
+    /**
+    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint a, uint b) internal pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint a, uint b) internal pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+}
